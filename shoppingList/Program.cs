564f2ebe85f8c471411d70709e2d87d6b3476afa
Maksim Muravjov <mmuravjo@tktk.ee>
2021-12-06 12:42:49 +0200

    new project

---
+++ shoppingList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace shoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\admin\samples";
+            string newDirectory = @"shoppingList";
+            string fileName = @"myShoppingList.txt";
+
+            Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+            File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+        }
+    }
+}
